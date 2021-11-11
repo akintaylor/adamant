47152e4e042d591b85a08d3e3d75800deee4aadf
akinbowale <akinbowale.akt@gmail.com>
2021-11-11 05:39:46 +0100

    feat: most_active_modules_by_churn_in_directory_from_last_six_months

--- adamant/__init__.py
+++ adamant/__init__.py
@@ -19,6 +19,7 @@ class Adamant:
         self.local_repos_dir_path = './.repos'
         self.repo_branch = branch
         self.root_dir = Path().resolve()
+        self.owd = os.getcwd()
 
         if self._is_cloned():
             self._set_repo_root_directory()
@@ -33,6 +34,10 @@ class Adamant:
 
     def _set_repo_root_directory(self):
         os.chdir(f'{self.local_repos_dir_path}/{self.repo_name}')
+
+    def _return_to_repo_root_dir(self):
+        os.chdir(self.owd)
+        self._set_repo_root_directory()
 
     @staticmethod
     def _pull_update():
@@ -54,20 +59,25 @@ class Adamant:
                                             days=-day)
         return str(proc_dt), str(past_date)
 
+    @staticmethod
+    def get_sub_dirs(directory_name):
+        os.chdir(f'{directory_name}')
+        dirs_in_curdir = list(filter(os.path.isdir, os.listdir(os.curdir)))
+
+        return dirs_in_curdir
+
     def most_active_modules_by_commits_in_directory_from_last_six_months(
             self,
             directory_name='.'
     ):
 
-        os.chdir(f'{directory_name}')
-        directories_in_curdir = \
-            list(filter(os.path.isdir, os.listdir(os.curdir)))
-
         present_date, past_date = self.get_present_and_past_date(month=6)
 
         commits = dict()
 
-        for directory in directories_in_curdir:
+        dirs_in_curdir = self.get_sub_dirs(directory_name)
+
+        for directory in dirs_in_curdir:
             commit_count_string = subprocess.check_output(
                 f"git log --oneline --follow --after='{past_date} 00:00' "
                 f"--before='{present_date} 23:59' -- {directory} | wc -l",
@@ -81,8 +91,7 @@ class Adamant:
 
         twelve_most_active_modules = sorted_commits[:12]
 
-        x_axis = []
-        y_axis = []
+        x_axis, y_axis = [], []
 
         for x, y in twelve_most_active_modules:
             x_axis.append(x)
@@ -102,5 +111,76 @@ class Adamant:
             os.mkdir(f'{self.root_dir}/.images')
 
         fig.savefig(
-            f'{self.root_dir}/.images/num_commits.png', bbox_inches='tight'
+            f'{self.root_dir}/.images/{self.repo_name}_num_commits.jpeg',
+            bbox_inches='tight'
         )
+
+        self._return_to_repo_root_dir()
+
+    def most_active_modules_by_churn_in_directory_from_last_six_months(
+            self,
+            directory_name='.'
+    ):
+
+        present_date, past_date = self.get_present_and_past_date(month=6)
+
+        churns = dict()
+
+        dirs_in_curdir = self.get_sub_dirs(directory_name)
+
+        for directory in dirs_in_curdir:
+            churn_count = subprocess.check_output(
+                f"git log --numstat --format= \"$@\" "
+                f"--follow --after='{past_date} 00:00' "
+                f"--before='{present_date} 23:59' -- {directory} | awk "
+                '\'{ins += $1}{del += $2} END{print ""ins","del""}\'',
+                shell=True
+            ).decode().strip()
+
+            num_insert, num_delete = churn_count.split(',')
+
+            if not num_insert:
+                num_insert = 0
+
+            if not num_delete:
+                num_delete = 0
+
+            churns[directory] = (int(num_insert), int(num_delete))
+
+        sorted_churns = sorted(
+            churns.items(),
+            key=lambda kv: (kv[1][0] + kv[1][1], kv[0]),
+            reverse=True
+        )
+
+        twelve_most_active_modules = sorted_churns[:12]
+
+        labels, inserts, deletions = [], [], []
+
+        for label, (num_inserts, num_deletions) in twelve_most_active_modules:
+            labels.append(label)
+            inserts.append(num_inserts)
+            deletions.append(num_deletions)
+
+        fig, ax = plt.subplots()
+
+        plt.xticks(rotation=45)
+
+        ax.bar(labels, deletions,
+               label='Number of lines deleted', color='indianred')
+        ax.bar(labels, inserts, bottom=deletions,
+               label='Number of lines added', color='mediumseagreen')
+
+        ax.set_ylabel('Churn')
+        ax.set_title(f'Churn for each {directory_name}/ subdirectory')
+        ax.legend()
+
+        if not Path(f'{self.root_dir}/.images').exists():
+            os.mkdir(f'{self.root_dir}/.images')
+
+        fig.savefig(
+            f'{self.root_dir}/.images/{self.repo_name}_churn_num.jpeg',
+            bbox_inches='tight'
+        )
+
+        self._return_to_repo_root_dir()
